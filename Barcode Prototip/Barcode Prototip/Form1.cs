4cd760426686c3bc34ab5850bfb1e53e7ce2d10d
razvangk <49474545+razvangk@users.noreply.github.com>
2019-04-10 11:05:18 +0300

    Add files via upload

---
+++ Barcode Prototip/Barcode Prototip/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barcode_Prototip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string BarCode = txtBarCode.Text;
+            try
+            {
+                Zen.Barcode.Code128BarcodeDraw brCode = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+                pictureBox1.Image = brCode.Draw(BarCode, 60);
+            }
+            catch (Exception)
+            {
+
+                
+            }
+        }
+    }
+}
